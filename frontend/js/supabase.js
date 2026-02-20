cc79267bbc1cd9ed83497a64421821b92e3aa322
Ni154 <niedson.santosperola@gmail.com>
2026-02-20 12:28:24 -0300

    Update supabase.js

--- frontend/js/supabase.js
+++ frontend/js/supabase.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://xsltdaaewmxgbsaefbkt.supabase.co"
+const SUPABASE_KEY = "sb_publishable_raOx_48VeYM8YGaCh3eHoA_r4Iyr53x"
 
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+)
